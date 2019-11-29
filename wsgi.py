11d9e6aed1f1972f279e98cf61867dc2ad386b98
baldcodr <chukay360@gmail.com>
2019-11-29 11:50:29 +0100

    commit

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
-import os
+# import os
 
-
-from dotenv import load_dotenv
-project_folder = os.path.expanduser('/IBL')  # adjust as appropriate
-load_dotenv(os.path.join(project_folder, '.env'))
+# from dotenv import load_dotenv
+# project_folder = os.path.expanduser('/IBL')  # adjust as appropriate
+# load_dotenv(os.path.join(project_folder, '.env'))
